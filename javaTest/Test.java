e209163c92c04a3542cd1b42ca29b914c9543902
김승연 <tmddus3002@gmail.com>
2025-01-06 22:24:55 +0900

    Test: interface & class relation Study

---
+++ javaTest/Test.java
@@ -0,0 +1,8 @@
+package javaTest;
+
+public class Test {
+    public static void main(String[] args) {
+        TestClass tc = new TestClass();
+        tc.test();
+    }
+}
---
+++ javaTest/TestClass.java
@@ -0,0 +1,8 @@
+package javaTest;
+
+public class TestClass implements TestInterface{
+    @Override
+    public void test() {
+        System.out.println("test");
+    }
+}
