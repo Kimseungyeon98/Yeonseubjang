11d07ec1758918bc003dab74b2023443fb8c96ed
김승연 <tmddus3002@gmail.com>
2025-01-07 20:28:50 +0900

    Study: extends study

--- javaTest/TestClass.java
+++ javaTest/TestClass.java
@@ -5,4 +5,14 @@ public class TestClass implements TestInterface{
     public void test() {
         System.out.println("test");
     }
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
