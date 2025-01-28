c0d723cab7f85f423dacc5e35268a1bd07f9f8b9
김승연 <tmddus3002@gmail.com>
2025-01-29 01:36:37 +0900

    최소비용 구하기 진행중

--- CodingTest.java
+++ CodingTest.java
@@ -1,11 +1,70 @@
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class CodingTest {
     public static void main(String[] args) {
-        System.out.println(solution("Bcad"));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n+1][n+1];
+        boolean[] visited = new boolean[n+1];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(arr[i],Integer.MAX_VALUE);
+        }
+        for(int i=0; i<m; i++){
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+            int v = sc.nextInt();
+            arr[s][e] = v;
+            arr[e][s] = v;
+        }
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        // 입력 받은 값 확인 시작
+        /*System.out.println("도시의 개수 : " + n);
+        System.out.println("버스의 개수 : " + m);
+        System.out.println("출발 도시의 번호, 도착 도시의 번호, 버스 비용");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+        System.out.println("출발하고자 하는 도시의 번호 : " + start);
+        System.out.println("도착하고자 하는 도시의 번호 : " + end);*/
+        // 입력 받은 값 확인 끝
+
+        // 알고리즘 구현 시작
+        visited[start] = true;
+        for(int i=0; i<n-1; i++){
+
+        }
     }
-    public static String solution(String my_string) {
-        return Arrays.stream(my_string.split("")).map(String::toLowerCase).sorted().reduce("", String::concat);
+    // 한 배열의 최소값 중 방문하지 않은 인덱스를 반환
+    static int minValue(int[] arr, boolean[] visited) {
+        int min = Integer.MAX_VALUE;
+        //최소값 찾기
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+        //최소값 중 방문하지 않은 인덱스를 반환하고 방문 처리
+        for(int i=0; i<arr.length; i++){
+            if(!visited[i] && arr[i]==min){
+                visited[i] = true;
+                return i;
+            }
+        }
+        return -1;
     }
 }
+
+
+/*
+* 도시의 개수(n)
+* 버스의 개수(m)
+* m+2
+* 출발 도시의 번호, 도착 도시의 번호, 버스 비용
+* 출발점의 도시번호, 도착점의 도시번호
+* */
